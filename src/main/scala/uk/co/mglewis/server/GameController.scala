8f7afce5ea052791e1f1619b70da5f4c8fdd27bc
matt.lewis <matthew.lewis@footballradar.com>
2020-04-11 12:43:34 +0100

    added properties file and associated changes to read it in

--- src/main/scala/uk/co/mglewis/server/GameController.scala
+++ src/main/scala/uk/co/mglewis/server/GameController.scala
@@ -4,13 +4,17 @@ import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 import uk.co.mglewis.core.Dictionary
 
-class GameController extends Controller {
+class GameController(secretPath: String) extends Controller {
 
   private val dictionary = new Dictionary("resources/word_list.txt")
 
   get("/ping") { request: Request =>
     info("ping")
     "Pong " + request.params.getOrElse("name", "unknown")
+  }
+
+  get(s"/$secretPath/test") { request: Request =>
+    "Secret found."
   }
 
 }
--- src/main/scala/uk/co/mglewis/server/Server.scala
+++ src/main/scala/uk/co/mglewis/server/Server.scala
@@ -1,14 +1,27 @@
 package uk.co.mglewis.server
 
+import java.util.Properties
+
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
+
+import scala.io.Source
 
 object ServerMain extends Server
 
 class Server extends HttpServer {
 
-  override protected def configureHttp(router: HttpRouter): Unit = {
-    router.add(new GameController)
+  val properties: Properties = {
+    val props = new Properties()
+    props.load(Source.fromFile("resources/properties").bufferedReader)
+    props
   }
+
+  val secretPath = properties.getProperty("TELEGRAM_SECRET_API_PATH")
+
+  override protected def configureHttp(router: HttpRouter): Unit = {
+    router.add(new GameController(secretPath))
+  }
+
 
 }
